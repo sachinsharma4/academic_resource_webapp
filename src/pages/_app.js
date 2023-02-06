1b09f1347deea70b55fd9b9d02fd0e71f1500b62
sachin <sachinshaarma7673@gmail.com>
2023-02-06 13:06:17 +0530

    adsense integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import Script from "next/script";
 import Nav from './nav'
 
 export default function App({ Component, pageProps }) {
-  return <><Nav/><Component {...pageProps} /></>
+  return <>
+  <Script id="Adsense-id" 
+  crossorigin="anonymous"
+  async strategy="afterInteractive"
+  onError={ (e) => { console.error('Script failed to load', e) }}
+src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8097535607316189"
+/>
+  <Nav/><Component {...pageProps} /></>
 }
