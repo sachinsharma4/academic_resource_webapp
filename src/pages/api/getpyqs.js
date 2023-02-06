8c174511bd3b3fbdd87b65ba5842eb454800dba2
sachin <sachinshaarma7673@gmail.com>
2023-02-06 12:32:20 +0530

    bug fix

--- src/pages/api/getpyqs.js
+++ src/pages/api/getpyqs.js
@@ -6,13 +6,13 @@ import connectdb from 'pyq/middelware';
 mongoose.set('strictQuery', false);
 
 
-const handler = async (req,res) => {
-   
-   await connectdb();
-   
-    res.status(200).json(await Pyq.find(req.query).catch((err)=> res.status(400).json({ err }))
-    )
-    console.log(req)
-    console.log(res)
+async function handler(req, res) {
+
+    await connectdb();
+
+    res.status(200).json(await Pyq.find(req.query).catch((err) => res.status(400).json({ err }))
+    );
+    console.log(req);
+    console.log(res);
 }
 export default handler
