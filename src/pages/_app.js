9efb8be949720eba98707ffdffef33c5690cb15e
sachinsharma4 <66316872+sachinsharma4@users.noreply.github.com>
2023-05-12 23:32:37 +0530

    Bug fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,11 +4,11 @@ import Nav from './nav'
 
 export default function App({ Component, pageProps }) {
   return <>
-  /*<Script id="Adsense-id" 
+ { /*<Script id="Adsense-id" 
   crossorigin="anonymous"
   async strategy="afterInteractive"
   onError={ (e) => { console.error('Script failed to load', e) }}
 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8097535607316189"
-/>*/
+/>*/} 
   <Nav/><Component {...pageProps} /></>
 }
